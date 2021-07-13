1f576f2837f4a359a7974ad47f38afc93331da16
Vsevolod Novak <v.novak@creatio.com>
2021-07-13 06:51:17 +0300

    1

---
+++ StartApplicationPool.ps1
@@ -0,0 +1,41 @@
+Param (
+	[Parameter (Mandatory=$true, Position=1)]
+	[string]$WebServer,
+	[Parameter (Mandatory=$true, Position=2)]
+	[string]$ApplicationPool
+)
+
+Write-Host "Trying to start application pool: $ApplicationPool"
+
+$WebServerСommand = {
+	param (
+		$ApplicationPool
+	)
+	
+	Import-Module WebAdministration
+	
+	Write-Output "Starting application pool $ApplicationPool"
+	
+	$PoolState = (Get-WebAppPoolState -Name $ApplicationPool).Value
+	Write-Output "Current application pool state is `'$PoolState`'"
+	if ($PoolState -ne "Started") {
+		$Counter = 0
+		do {
+			$Counter += 1
+			if ($Counter -gt 120) {
+				throw "Application pool is not start too long"
+			}
+			Stop-WebAppPool -Name $ApplicationPool
+			Write-Output "Waiting until application pool start 5 seconds..."
+			Start-Sleep -Seconds 5
+			Write-Output "Getting application pool `'$ApplicationPool`' state information"
+			$PoolState = (Get-WebAppPoolState -Name $ApplicationPool).Value
+			Write-Output "Current application pool state is `'$PoolState`'"
+		} while ($PoolState -ne "Started")
+	}
+	
+	Write-Output "Application pool $ApplicationPool started successfully!"
+}
+
+
+Invoke-Command -ComputerName $WebServer -Command $WebServerСommand -ArgumentList $ApplicationPool
