53bf0fe9fd161f19e7e8b1a6addfd5416b1e4ccd
Vsevolod Novak <v.novak@creatio.com>
2021-07-13 06:35:46 +0300

    1

--- StopApplicationPool.ps1
+++ StopApplicationPool.ps1
@@ -1,1 +1,45 @@
-Write-Host "Hello From Powershell in Jenkins!"
+Param (
+	[Parameter (Mandatory=$true, Position=1)]
+	[string]$WebServer,
+	[Parameter (Mandatory=$true, Position=2)]
+	[string]$ApplicationPool
+)
+
+Write-Host "Trying to stop application pool: $ApplicationPool"
+
+if ($AppConfig -eq $Null) {
+	$AppConfig = Get-AppConfig $Site
+}
+
+$WebServerСommand = {
+	param (
+		$ApplicationPool
+	)
+	
+	Import-Module WebAdministration
+	
+	Write-Output "Stopping application pool $ApplicationPool"
+	
+	$PoolState = (Get-WebAppPoolState -Name $ApplicationPool).Value
+	Write-Output "Current application pool state is `'$PoolState`'"
+	if ($PoolState -ne "Stopped") {
+		$Counter = 0
+		do {
+			$Counter += 1
+			if ($Counter -gt 120) {
+				throw "Application pool is not stopped too long"
+			}
+			Stop-WebAppPool -Name $ApplicationPool
+			Write-Output "Waiting until application pool is stopped 5 seconds..."
+			Start-Sleep -Seconds 5
+			Write-Output "Getting application pool `'$ApplicationPool`' state information"
+			$PoolState = (Get-WebAppPoolState -Name $ApplicationPool).Value
+			Write-Output "Current application pool state is `'$PoolState`'"
+		} while ($PoolState -ne "Stopped")
+	}
+	
+	Write-Output "Application pool $ApplicationPool stoped successfully!"
+}
+
+
+Invoke-RemoteCommand -Server $WebServer -Command $WebServerСommand -ArgumentList $ApplicationPool
